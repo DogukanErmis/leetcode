e55ad0f5556333b585dca8a598f24de13f287d42
dogukanermis <ermisd+github@pm.me>
2021-11-25 18:58:35 +0100

    add Two Sum problem

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
 // 1. TWO SUM
 // Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target
 
-// First solution (Bruteforce)
 /**
  * @param {number[]} nums
  * @param {number} target
  * @return {number[]}
  */
 const twoSum = function (nums, target) {
-  for (let i = 0; i < nums.length - 1; i++) {
-    for (let j = i + 1; j < nums.length; j++) {
-      if (nums[i] + nums[j] === target) return [i, j];
+  // Bruteforce
+  // Time complexity: O(n^^2)
+  // Space complexity: O(1)
+  // for (let i = 0; i < nums.length - 1; i++) {
+  //   for (let j = i + 1; j < nums.length; j++) {
+  //     if (nums[i] + nums[j] === target) return [i, j];
+  //   }
+  // }
+
+  // Hash Map
+  // Time complexity: O(n)
+  // Space complexity: O(n)
+  const map = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    map.set(nums[i], i);
+  }
+
+  for (let i = 0; i < nums.length; i++) {
+    let complement = target - nums[i];
+    if (map.has(complement)) {
+      return [i, map.get(complement)];
     }
   }
+  return null;
 };
 
-console.log(twoSum([3, 4, 5, 2, 1], 8));
+console.log(twoSum([3, 4, 5, 2, 1], 9));
