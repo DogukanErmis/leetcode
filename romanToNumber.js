936c813d5101b1ee8635a0e49f7fbbe0ecb076d6
dogukanermis <ermisd+github@pm.me>
2021-11-27 19:46:44 +0100

    add romanToNumber exercice

---
+++ romanToNumber.js
@@ -0,0 +1,32 @@
+// Given a roman numeral, convert it to an integer.
+// Symbol       Value
+// I             1
+// V             5
+// X             10
+// L             50
+// C             100
+// D             500
+// M             1000
+
+const romanToNumber = (s) => {
+  const map = new Map([
+    ["I", 1],
+    ["V", 5],
+    ["X", 10],
+    ["L", 50],
+    ["C", 100],
+    ["D", 500],
+    ["M", 1000],
+  ]);
+
+  let result = 0;
+  for (let i = 0; i < s.length; i++) {
+    const curr = map.get(s[i]);
+    const next = map.get(s[i + 1]);
+    if (curr < next) result -= curr;
+    else result += curr;
+  }
+  return result;
+};
+
+console.log(romanToNumber("IV"));
